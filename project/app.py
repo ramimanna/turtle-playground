5ab40272d9f83e7b69f3ed5c1bc0697927ada97c
amin10 <manna@mit.edu>
2016-08-22 17:43:02 +0300

    added POST /submit_code route, and some basic user identification and matching into two player games

--- project/app.py
+++ project/app.py
@@ -1,19 +1,45 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, request, Response
+import json
 
+app = Flask(__name__)
 app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 app.debug = True
 
+waiting = []
+games = {}
+class Game:
+    def __init__(self, player1, player2):
+        pass
 
 @app.route('/')
 def index():
     return render_template('index.jade')
 
-
 @app.route('/hello')
 def hello():
     return render_template('hello.jade')
 
+@app.route('/submit_code', methods=['post'])
+def player1():
+    code = request.form['code']
+    player_id = request.form['id']
+
+    if player_id in games:
+        game = games[player_id]
+        resp = "welcome back"
+    elif player_id in waiting:
+        resp = "keep waiting"
+    elif waiting:
+        other_player_id = waiting.pop()
+        g = Game(player_id, other_player_id)
+        games[other_player_id] = g
+        games[player_id] = g
+        resp = "matched!"
+    else:
+        waiting.append(player_id)
+        resp="new player! Please wait to be matched"
+
+    return Response(json.dumps(resp), status=200, mimetype='application/json')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ project/game.py
@@ -0,0 +1,16 @@
+import turtle
+
+p1 = turtle.clone()
+p2 = turtle.clone()
+
+
+"""User submitted code:
+def play_turn_1(p1):
+    pass
+
+def play_turn_2(p2):
+    pass
+"""
+
+play_turn_2()
+play_turn_1()
