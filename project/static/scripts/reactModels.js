9167eea44f75333a21a62ab3634f58ea48ca8259
Rami <rami15manna@gmail.com>
2016-08-22 16:54:07 +0300

    created first React Element: CodeForm, and added script tags for React

--- project/static/scripts/reactModels.js
+++ project/static/scripts/reactModels.js
@@ -1,5 +1,18 @@
 var CodeForm = React.createClass({
   render: function(){
+    return(
+      <form> 
+        <textarea id="yourcode" cols="40" rows="10">
 
+        </textarea><br /> 
+      <button type="button" onclick="runit()">Run</button> 
+      </form> 
+    );
   }
 });
+
+if(!window.react){
+  window.react = {};
+}
+
+window.react.CodeForm = CodeForm;
