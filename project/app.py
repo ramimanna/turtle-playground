f4f4bc7357fa6d9c592d75bca28a2841fa51bbd1
amin10 <manna@mit.edu>
2016-08-23 01:32:10 +0300

    backend Player logic. new route POST /start_game

--- project/app.py
+++ project/app.py
@@ -7,11 +7,37 @@ app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 app.debug = True
 socketio = SocketIO(app)
 
-waiting = []
-games = {}
+waiting = [] # player_ids
+games = {} # player_id : Game containing player
+
+class Player:
+    def __init__(self, player_id):
+        self.id = player_id
+        self.code = None
+
 class Game:
     def __init__(self, player1, player2):
-        pass
+        self.player1 = player1
+        self.player2 = player2
+    
+    def play_turn(self, player_id, player_code):
+        if player_id == self.player1.id:
+            player = self.player1
+            partner = self.player2
+        elif player_id == self.player2.id:
+            player = self.player2
+            partner = self.player1
+
+        if player.code is None:
+            player.code = player_code
+        else:
+            pass # Player is resubmitting code during same turn.
+
+        if player.code and partner.code:
+            return "turn" # Todo inform client, and reset player.code and partner.code
+        else:
+            return "wait for partner"
+
 
 @app.route('/')
 def index():
@@ -21,9 +47,8 @@ def index():
 def hello():
     return render_template('hello.jade')
 
-@app.route('/submit_code', methods=['post'])
-def submit_code():
-    code = request.form['code']
+@app.route('/start_game', methods=['post'])
+def start_game():
     player_id = request.form['id']
 
     if player_id in games:
@@ -32,16 +57,30 @@ def submit_code():
     elif player_id in waiting:
         resp = "keep waiting"
     elif waiting:
-        other_player_id = waiting.pop()
-        g = Game(player_id, other_player_id)
-        games[other_player_id] = g
+        player1_id = waiting.pop()
+        g = Game(Player(player1_id), Player(player_id))
+
+        games[player1_id] = g
         games[player_id] = g
         resp = "matched!"
     else:
         waiting.append(player_id)
         resp="new player! Please wait to be matched"
 
-    return Response(json.dumps(resp), status=200, mimetype='application/json')
+    return Response(json.dumps(resp), status=200, mimetype='application/json')   
+
+@app.route('/submit_code', methods=['post'])
+def submit_code():
+    player_id = request.form['id']
+    player_code = request.form['code']
+
+    if player_id not in games:
+        return Response(json.dumps("wait"), status=404, mimetype='application/json')
+
+    else:
+        g = games[player_id]
+        resp = g.play_turn(player_id, player_code)
+        return Response(json.dumps(resp), status=200, mimetype='application/json')
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
