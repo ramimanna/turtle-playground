54e66b1394055dac8807236c9d40c251de0a945c
Rami <rami15manna@gmail.com>
2016-08-22 18:38:09 +0300

    Posted code and id (0 for now) and recieved message back

--- project/static/scripts/reactModels.js
+++ project/static/scripts/reactModels.js
@@ -1,4 +1,39 @@
 var CodeForm = React.createClass({
+  getInitialState: function(){
+    return {code: '',id: 1};
+  },
+  handleCodeChange: function(e){
+    this.setState({code: e.target.value});
+  },
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+    console.log("handleSubmit happening");
+    var code = this.state.code.trim();
+    var id = this.state.id;
+    if (!code || !id) {
+      return;
+    }
+    this.props.onCodeSubmit({code: code, id: id});
+    this.setState(this.getInitialState());
+  },
+  render: function(){
+    return(
+      <form onSubmit={this.handleSubmit}> 
+        <h1>Message {this.props.message}</h1>        
+        <textarea id="yourcode" onChange={this.handleCodeChange} cols="40" rows="10">
+
+        </textarea><br />
+      <button type="submit">Run</button> 
+      </form> 
+    );
+  }
+});
+
+var PlayerBox = React.createClass({
+  getInitialState: function(){
+    return({data:''});
+  },
   outf: function(text) {
     // output functions are configurable.  This one just appends some text
     // to a pre element.
@@ -11,7 +46,7 @@ var CodeForm = React.createClass({
     return Sk.builtinFiles["files"][x];
   },
 
-  runit: function(){
+  runit: function(paddedCode){
     // Here's everything you need to run a python program in skulpt
     // grab the code from your textarea
     // get a reference to your pre element for output
@@ -20,7 +55,7 @@ var CodeForm = React.createClass({
 
     console.log("RUN IT!");
 
-    var prog = document.getElementById(this.props.codeID).value; 
+    var prog = document.getElementById("yourcode").value;
     var mypre = document.getElementById(this.props.outputID); 
     mypre.innerHTML = ''; 
     Sk.pre = this.props.outputID;
@@ -36,23 +71,30 @@ var CodeForm = React.createClass({
        console.log(err.toString());
     }); 
   },
-  handleSubmit: function(e){
-    e.preventDefault();
-    this.runit();
-  },
+
+  handleCodeSubmit: function(turnData){
+    console.log("turnData");
+    console.log(turnData);
+    $.ajax({
+      url: this.props.url,
+      // dataType: 'json',
+      type: 'POST',
+      data: turnData,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },  
+
   render: function(){
     return(
-      <form onSubmit={this.handleSubmit}> 
-        <textarea id={this.props.codeID} cols="40" rows="10">
-
-        </textarea><br /> 
-      <button type="submit">Run</button> 
-      </form> 
+      <CodeForm message = {this.state.data} onCodeSubmit={this.handleCodeSubmit} />
     );
   }
 });
-
-
 
 
 
@@ -61,3 +103,4 @@ if(!window.react){
 }
 
 window.react.CodeForm = CodeForm;
+window.react.PlayerBox = PlayerBox;
